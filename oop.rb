380ac1591edecf6bf8e54ffcd072775d8c442164
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-01 15:43:29 -0600

    add the check age instance method and make age adynamic attribute

--- oop.rb
+++ oop.rb
@@ -5,8 +5,8 @@
 
 class Unicorn
     attr_reader :name, :color
-    def initialize(uname, color = "silver")
-        @uname = uname
+    def initialize(name, color = "silver")
+        @name = name
         @color = color 
         #I opted to make color a flexible attribute. 
     end
@@ -29,10 +29,11 @@ puts "\n"
 #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
+
 class Vampire
-    attr_reader :vname, :pet, :thirsty 
-    def initialize(vname, pet = "bat")
-        @vname = vname
+    attr_reader :name, :pet, :thirsty 
+    def initialize(name, pet = "bat")
+        @name = name
         @pet = pet 
         @thirsty = true
 
@@ -57,10 +58,10 @@ p vampire1
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
 class Dragon
-    attr_reader :dname, :rider, :color, :is_hungry
+    attr_reader :name, :rider, :color, :is_hungry
 
-    def initialize(dname, rider, color, is_hungry=true)
-        @dname = dname
+    def initialize(name, rider, color, is_hungry=true)
+        @name = name
         @rider = rider
         @color = color
         @is_hungry = is_hungry
@@ -98,18 +99,18 @@ p dragon1
  
 class Hobbit 
 
-    attr_reader :hname, :disposition, :age, :is_adult, :is_old, :has_ring
-    def initialize(hname, disposition)
-        @hname = hname
+    attr_reader :name, :disposition, :age, :is_adult, :is_old, :has_ring
+    def initialize(name, disposition, age)
+        @name = name
         @disposition = disposition
-        @age = 0
+        @age = age
         @is_adult = false
         @is_old = false
         @has_ring = false
     end
 
         def has_ring
-        if @hname == "Frodo"
+        if @name == "Frodo"
             @has_ring = true
         end 
     end
@@ -120,17 +121,33 @@ class Hobbit
         if age >= 101
             @is_old = true
          
-
         elsif age >= 33
             @is_adult = true
         end 
     end
+
+    def check_age
+        if age >= 101
+            @is_old = true
+         
+        elsif age >= 33
+            @is_adult = true
+        end 
+    end 
 end 
 
-hobbit1 = Hobbit.new("Frodo", "Adventerous")
+hobbit1 = Hobbit.new("Frodo", "Adventerous", 32)
+hobbit2 = Hobbit.new("Pippen", "Jester", 100)
 
 p hobbit1
+hobbit1.check_age
 hobbit1.has_ring
 hobbit1.celebrate_birthday
 hobbit1.celebrate_birthday
 p hobbit1
+
+p hobbit2
+hobbit2.check_age
+hobbit2.has_ring
+hobbit2.celebrate_birthday
+p hobbit2
